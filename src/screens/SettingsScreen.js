9e6f14e2eb344d9709fdd57889cc7c386966cea4
Ramya Challa <ramyac6@cs.washington.edu>
2020-09-12 17:58:36 -0700

    settings updates

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import { firebase } from "../config/firebaseConfig";
+import Colors from "../constants/Colors";
 
 export default function SettingsScreen(props, { navigation }) {
   const usersRef = firebase.firestore().collection("users");
@@ -27,11 +28,25 @@ export default function SettingsScreen(props, { navigation }) {
   }, []);
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1 }}>
       <View style={styles.welcome}>
         <Text style={{ fontSize: 24 }}>Welcome back {userName}!</Text>
       </View>
-      <Text>Settings Screen</Text>
+      <View style={styles.items}>
+        <Text style={{ fontSize: 18 }}>Preferences: breakfast, dessert</Text>
+      </View>
+      <View style={styles.items}>
+        <Text style={{ fontSize: 18 }}>Location: Seattle</Text>
+      </View>
+      <View style={styles.items}>
+        <Text style={{ fontSize: 18 }}>Age: 19</Text>
+      </View>
+      <View style={styles.items}>
+        <Text style={{ fontSize: 18 }}>Cooking Style: Experimental</Text>
+      </View>
+      <View style={styles.logout}>
+        <Text style={{ fontSize: 24 }}>Logout</Text>
+      </View>
     </View>
   );
 }
@@ -40,6 +55,21 @@ const styles = StyleSheet.create({
   welcome: {
     backgroundColor: "lightblue",
     marginBottom: 10,
+    alignItems: "center",
+    padding: 10,
+  },
+  items: {
+    backgroundColor: Colors.lightGray,
+    marginBottom: 10,
+    padding: 10,
+    alignItems: "center",
+    borderColor: Colors.darkGray,
+    borderRadius: 2,
+  },
+  logout: {
+    backgroundColor: Colors.lightBlue,
+    marginBottom: 10,
+    justifyContent: "flex-end",
     alignItems: "center",
     padding: 10,
   },
