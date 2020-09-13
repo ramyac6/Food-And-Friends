b03aee67b1631aa99b763d477dd2be981509cc4e
Ramya Challa <ramyac6@cs.washington.edu>
2020-09-12 17:35:51 -0700

    added markers to map

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,20 +1,31 @@
 import * as React from "react";
 import { View, Text } from "react-native";
 
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 export default function MapScreen({ navigation }) {
+  var John = {
+    latitude: 46.78825,
+    longitude: -122.4324,
+  };
+  var Mary = {
+    latitude: 47.78825,
+    longitude: -121.4324,
+  };
+  var Jimmy = {
+    latitude: 46.48825,
+    longitude: -121.4324,
+  };
+
   return (
-    <MapView
-      style={{ flex: 1 }}
-      provider={PROVIDER_GOOGLE}
-      showsUserLocation
-      //initialRegion={{
-      //  latitude: 37.78825,
-      //  longitude: -122.4324,
-      //   latitudeDelta: 0.0922,
-      //  longitudeDelta: 0.0421,
-      //}}
-    />
+    <MapView style={{ flex: 1 }} provider={PROVIDER_GOOGLE} showsUserLocation>
+      <Marker coordinate={John} title={"John"} description={"(123) 456-7890"} />
+      <Marker coordinate={Mary} title={"Mary"} description={"(987) 456-1234"} />
+      <Marker
+        coordinate={Jimmy}
+        title={"Jimmy"}
+        description={"(765) 123-4567"}
+      />
+    </MapView>
   );
 }
